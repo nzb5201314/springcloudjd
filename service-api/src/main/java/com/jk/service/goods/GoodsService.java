8e5e4209dc390cbae7d5a4061971b85dcf4702df
xmg666 <1332570209@qq.com>
2019-04-17 16:28:11 +0800

    2019-4-17-16-30

---
+++ service-api/src/main/java/com/jk/service/goods/GoodsService.java
@@ -0,0 +1,21 @@
+/**
+ * Copyright (C), 2019, 金科教育
+ * FileName: GoodsService
+ * Author:   mrq
+ * Date:     2019/4/17 16:22
+ * History:
+ * <author>          <time>          <version>          <desc>
+ * 作者姓名：mrq        修改时间           版本号              描述
+ */
+package com.jk.service.goods;
+
+/**
+ * 〈一句话功能简述〉<br> 
+ * 〈goods〉
+ *
+ * @author Administrator
+ * @create 2019/4/17
+ * @since 1.0.0
+ */
+public class GoodsService {
+}
